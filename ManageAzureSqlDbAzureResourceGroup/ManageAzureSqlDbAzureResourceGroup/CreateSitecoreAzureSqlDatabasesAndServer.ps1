9949a723df878a7428b880ef58ba23aa31ee927d
mathieu-benoit <mathieu-benoit@hotmail.fr>
2016-10-29 15:58:47 -0400

    Add ARM project and associated .json and .ps1 file to create the dbs.

---
+++ ManageAzureSqlDbAzureResourceGroup/ManageAzureSqlDbAzureResourceGroup/CreateSitecoreAzureSqlDatabasesAndServer.ps1
@@ -0,0 +1,12 @@
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] $TemplateFile = 'CreateSitecoreAzureSqlDatabasesAndServer.json'
+)
+
+Login-AzureRmAccount
+#Select-AzureSubscription "MySubscription"
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
+New-AzureRmResourceGroupDeployment -Name $ResourceGroupName `
+                                       -ResourceGroupName $ResourceGroupName `
+                                       -TemplateFile $TemplateFile
